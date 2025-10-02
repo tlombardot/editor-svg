e41285811db0e73bf713cedf9100a32c8f3b6833
Thomas Lombardot <thomas.lombardot@gmail.com>
2025-10-03 00:22:28 +0200

    feat: add function initShape & getCoord

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ typedef enum{
     SQUARE,
     LINE,
     POLYGONE,
+    GROUP,
+    WAYS,
+    STRLINE,
 
 }ShapeType;
 
@@ -46,50 +49,151 @@ typedef struct
 
 typedef struct 
 {
-    union
+
+}Ways;
+
+typedef struct 
+{
+
+}Group;
+
+typedef struct 
+{
+
+}Str_line;
+
+
+
+
+typedef struct 
+{
+    union main
     {
         Rectangle rect;
-        Circle circle;
+        Circle cirlce;
         Line line;
-        Square square;
+        Square squ;
         Ellipse elli;
         Polygone poly;
+        Str_line strline;
+        Group grp;
+        Ways ways;
+        
     }data;
     
+    
+
 }Shape;
 
 
-void initShape(Shape *shape,ShapeType type){
+
+Shape* initShape(ShapeType type){
+    Shape *shape = malloc(sizeof(Shape));
     switch (type)
     {
-    case RECTANGLE:
-        /* code */
+    case RECTANGLE: {
+        int x,y,width,height;
+        printf("What is your x coordinates ? (TOP LEFT)\n");
+        scanf("%d",&x);
+        shape->data.rect.x = x;
+        printf("What is your y coordinates ? (TOP LEFT)\n");
+        scanf("%d",&y);
+        shape->data.rect.y = y;
+        printf("What is your width (RIGHT)? (NEGATIVE OPPOSITE WAYS)\n");
+        scanf("%d",&width);
+        shape->data.rect.width = width;
+        printf("What is your height (DOWN)? (NEGATIVE OPPOSITE WAYS)\n");
+        scanf("%d",&height);
+        shape->data.rect.height = height;
+        return shape;
         break;
-    case CIRCLE:
-        /* code */
+    }
+    case CIRCLE: {
+        return NULL;
         break;
-    case LINE:
-        /* code */
+    }
+    case LINE: {
+        return NULL;
         break;
-    case ELLIPSE:
-        /* code */
+    }
+    case ELLIPSE: {
+        return NULL;
         break;
-    case SQUARE:
-        /* code */
+    }
+    case SQUARE: {
+        return NULL;
         break;
-    case POLYGONE:
-        /* code */
+    }
+    case POLYGONE: {
+        return NULL;
         break;
-    default:
+    }
+    case GROUP: {
+        return NULL;
         break;
+    }
+    case WAYS: {
+        return NULL;
+        break;
+    }
+    case STRLINE: {
+        return NULL;
+        break;
+    }
+    default: {
+        return NULL;
+        break;
+    }
+    
+    }
+}
+
+void getCoord(Shape* shape, ShapeType type){
+    switch (type)
+    {
+    case RECTANGLE: {
+        printf("/+--| Rectangle |--+/\n");
+        printf("Position (x,y) : %d,%d \n",shape->data.rect.x,shape->data.rect.y);
+        printf("Width : %d \n",shape->data.rect.width);
+        printf("Height : %d \n",shape->data.rect.height);
+        break;
+    }
+    case CIRCLE: {
+        break;
+    }
+    case LINE: {
+        break;
+    }
+    case ELLIPSE: {
+        break;
+    }
+    case SQUARE: {
+        break;
+    }
+    case POLYGONE: {
+        break;
+    }
+    case GROUP: {
+        break;
+    }
+    case WAYS: {
+        break;
+    }
+    case STRLINE: {
+        break;
+    }
+    default: {
+        break;
+    }
     }
 }
 
 
-
-
-
-
-
-int main (void){
+int main(void){
+    Shape *shape;
+    shape = initShape(RECTANGLE);
+    getCoord(shape,RECTANGLE);
+    return EXIT_SUCCESS;
 }
+
+

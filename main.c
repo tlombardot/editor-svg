f10d85eee2fb3672a3659b7dda9135aa9f5a8afe
Thomas Lombardot <thomas.lombardot@gmail.com>
2025-10-02 18:14:13 +0200

    feat: base project main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    int x;
+    int y;
+}coord;
+
+void initCoordinate(coord *shape){
+
+}
+
+int main (void){
+    return EXIT_SUCCESS;
+}
